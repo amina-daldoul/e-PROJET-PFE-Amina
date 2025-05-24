43f1d009a0980dc65ec4160066d0f452d4d70a92
youssef-zekri <youssef.essedik.zekri@gmail.com>
2025-05-24 02:42:28 +0100

    cool

--- src/modules/shared/utils/supabase.ts
+++ src/modules/shared/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  'https://uhipejbctrgcnwxedwxf.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoaXBlamJjdHJnY253eGVkd3hmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwMDc0NjksImV4cCI6MjA0NzU4MzQ2OX0.RlFm8Goi9TLoq1CrlNI9IVVM2Eh76PSXnH_qCgrxosM'
+  'https://vppqgrvpxgxygigomvyh.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwcHFncnZweGd4eWdpZ29tdnloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwNDgwNjcsImV4cCI6MjA2MzYyNDA2N30.uNSEiy6QLjiR7TfInQwzLXVqNcqvbFgjWppKYXGqgJc'
 )
 export { supabase }
